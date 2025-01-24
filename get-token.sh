7d30b6ebe050e15ef598efb20c3d16635d47473f
Georgi Panov <77702912+d4rkfella@users.noreply.github.com>
2025-01-24 21:56:15 +0200

    Update get-token.sh

--- get-token.sh
+++ get-token.sh
@@ -7,6 +7,10 @@ TOKEN=$(curl -X POST "$KEYCLOAK_URL" \
   -d "scope=$SCOPE" \
   -H "Content-Type: application/x-www-form-urlencoded" | jq -r .access_token)
 
-kubectl create secret generic oidc-jwt \
-  --from-literal=headers="Authorization: Bearer $TOKEN" \
-  --dry-run=client -o yaml | kubectl apply -f -
+echo -e "apiVersion: v1
+kind: Secret
+metadata:
+  name: oidc-jwt
+stringData:
+  headers: |
+    Authorization: Bearer $TOKEN" | kubectl apply -f -
