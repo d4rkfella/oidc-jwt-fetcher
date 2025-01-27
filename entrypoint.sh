1c8c213d1599874b36c262be276c248dce26e97d
Georgi Panov <georgi.panov@darkfellanetwork.com>
2025-01-27 17:33:37 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ -z "${$KEYCLOAK_URL}"         ||
+if [[ -z "${$TOKEN_ENDPOINT_URL}"         ||
       -z "${CLIENT_ID}"
 ]]; then
     printf "\e[1;32m%-6s\e[m\n" "Invalid configuration - missing a required environment variable"
-    [[ -z "${KEYCLOAK_URL}" ]]             && printf "\e[1;32m%-6s\e[m\n" "KEYCLOAK_URL: unset"
+    [[ -z "${TOKEN_ENDPOINT_URL}" ]]       && printf "\e[1;32m%-6s\e[m\n" "TOKEN_ENDPOINT_URL: unset"
     [[ -z "${CLIENT_ID}" ]]                && printf "\e[1;32m%-6s\e[m\n" "CLIENT_ID: unset"
     exit 1
 fi
@@ -15,13 +15,13 @@ if [[ ! -f "$CLIENT_SECRET_FILE" ]]; then
 fi
 
 if [[ -z "$SCOPE" ]]; then
-  TOKEN=$(curl -sS -X POST "$KEYCLOAK_URL" \
+  TOKEN=$(curl -sS -X POST "$TOKEN_ENDPOINT_URL" \
     -d "client_id=$CLIENT_ID" \
     -d "client_secret=$(cat "$CLIENT_SECRET_FILE")" \
     -d "grant_type=client_credentials" \
     -H "Content-Type: application/x-www-form-urlencoded" | jq -r .access_token)
 else
-  TOKEN=$(curl -sS -X POST "$KEYCLOAK_URL" \
+  TOKEN=$(curl -sS -X POST "$TOKEN_ENDPOINT_URL" \
     -d "client_id=$CLIENT_ID" \
     -d "client_secret=$(cat "$CLIENT_SECRET_FILE")" \
     -d "grant_type=client_credentials" \
